c4aa1a3f9bc463ea87cf9466361b5bcc615a685d
Tang Zhi Siang <siangztang@gmail.com>
2023-06-19 18:18:19 +0800

    commit

--- demo/src/main/java/com/example/Controller/PatientListController.java
+++ demo/src/main/java/com/example/Controller/PatientListController.java
@@ -1,12 +1,22 @@
 package com.example.Controller;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+
 import com.example.Admin;
 import com.example.Patient;
+import com.example.SwitchPage;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
@@ -15,8 +25,12 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 public class PatientListController {
+
+    @FXML
+    private StackPane pages;
 
     @FXML
     private ResourceBundle resources;
@@ -70,9 +84,6 @@ public class PatientListController {
     private TableColumn<Patient, String> patIDCol;
 
     @FXML
-    private TextField patIDField;
-
-    @FXML
     private TableView<Patient> patListTable;
 
     @FXML
@@ -97,29 +108,78 @@ public class PatientListController {
     private Button updateBtn;
 
     @FXML
-    void initialize() {
-        
-    }
+    void initialize(){
+        manageDptBtn.setOnAction(event -> {
+            SwitchPage.switchPage(event, manageDptBtn);
+        });
 
-    public void initData(Admin admin){
-        unameLabel.setText(admin.getUname());
+        manageDocBtn.setOnAction(event -> {
+            SwitchPage.switchPage(event, manageDocBtn);
+        });
+
+        manageMedicineBtn.setOnAction(event -> {
+            SwitchPage.switchPage(event, manageMedicineBtn);
+        });
         patGenderBox.getItems().addAll("M", "F");
         resetBtn.setOnAction(event -> {
             resetBtnAction();
         });
         unFocusAll();
+        patListTable.getColumns().forEach(e -> e.setReorderable(false));
+        patListTable.setOnMouseClicked(event -> {
+            if (event.getClickCount() == 1) {
+                Patient selectedPatient = patListTable.getSelectionModel().getSelectedItem();
+                if (selectedPatient != null) {
+                    patNameField.setText(selectedPatient.getName());
+                    patICField.setText(String.valueOf(selectedPatient.getIc()));
+                    patCotField.setText(String.valueOf(selectedPatient.getContact_info()));
+                    patDepartmentField.setValue(selectedPatient.getDepartment());
+                    patGenderBox.setValue(String.valueOf(selectedPatient.getGender()));
+
+                }
+            } else if (event.getClickCount() == 2) {
+                Patient selectedPatient = patListTable.getSelectionModel().getSelectedItem();
+                if (selectedPatient != null) {
+                    // Do something with the selected patient data
+                    System.out.println("Selected patient ID: " + selectedPatient.getPatient_id());
+                    try {
+                        FXMLLoader loader = new FXMLLoader();
+                        Parent root = loader.load(new FileInputStream("demo\\src\\main\\resources\\com\\example\\PatientHistory.fxml"));
+                        Scene scene = new Scene(root);
+                        Stage stage = new Stage();
+                        stage.setScene(scene);
+                        stage.show();
+                        PatientHistoryController controller = loader.getController();
+                        controller.initData(admin, selectedPatient.getPatient_id());
+                        Node node = (Node) event.getSource();
+                        Stage currentStage = (Stage) node.getScene().getWindow();
+                        currentStage.close();
+                        
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        patientShowListData();
+    }
+
+    private Admin admin;
+
+
+    public void initData(Admin admin){
+        this.admin = admin;
+        unameLabel.setText(admin.getUname());
     }
 
     public void resetBtnAction(){
-        patIDField.setText("");
         patNameField.setText("");
         patICField.setText("");
         patCotField.setText("");
         patDepartmentField.setValue("");
         patGenderBox.setValue("");
         searchField.setText("");
-        unFocusAll();
-        
+        patListTable.getSelectionModel().clearSelection();        
     }
 
     public void unFocusAll(){
@@ -128,7 +188,6 @@ public class PatientListController {
         manageMedicineBtn.setFocusTraversable(false);
         patListTable.setFocusTraversable(false);
         searchField.setFocusTraversable(false);
-        patIDField.setFocusTraversable(false);
         patNameField.setFocusTraversable(false);
         patICField.setFocusTraversable(false);
         patCotField.setFocusTraversable(false);
@@ -140,23 +199,21 @@ public class PatientListController {
         resetBtn.setFocusTraversable(false);
     }
 
-    
-    
-
-    // private ObservableList<Patient> patientList;
-
-
     public void patientShowListData(){
-        // patientList = 
-        patIDCol.setCellValueFactory(new PropertyValueFactory<>("patID"));
-        patNameCol.setCellValueFactory(new PropertyValueFactory<>("patName"));
-        patICCol.setCellValueFactory(new PropertyValueFactory<>("patIC"));
-        patAgeCol.setCellValueFactory(new PropertyValueFactory<>("patAge"));
-        patGenderCol.setCellValueFactory(new PropertyValueFactory<>("patGender"));
-        patCotInfoCol.setCellValueFactory(new PropertyValueFactory<>("patCotInfo"));
-        patDepartmentCol.setCellValueFactory(new PropertyValueFactory<>("patDepartment"));
+
+        ObservableList<Patient> listData = FXCollections.observableArrayList();
+        listData.add(new Patient("P0001", "John", 12345612234L, 20, 'M', 12345L, "Emergency"));
+        listData.add(new Patient("P0002", "Mary", 12345612234L, 20, 'F', 12345L, "Emergency"));
+
+        patIDCol.setCellValueFactory(new PropertyValueFactory<>("patient_id"));
+        patNameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        patICCol.setCellValueFactory(new PropertyValueFactory<>("ic"));
+        patAgeCol.setCellValueFactory(new PropertyValueFactory<>("age"));
+        patGenderCol.setCellValueFactory(new PropertyValueFactory<>("gender"));
+        patCotInfoCol.setCellValueFactory(new PropertyValueFactory<>("contact_info"));
+        patDepartmentCol.setCellValueFactory(new PropertyValueFactory<>("department"));
         
-        // patListTable.setItems(patientList);
+        patListTable.setItems(listData);
 
     }
 
