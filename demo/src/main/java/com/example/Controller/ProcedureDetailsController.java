36cabca06250886e92388681fa9d6ac1ebd5236e
Tang Zhi Siang <siangztang@gmail.com>
2023-06-20 22:36:36 +0800

    update procedure details

--- demo/src/main/java/com/example/Controller/ProcedureDetailsController.java
+++ demo/src/main/java/com/example/Controller/ProcedureDetailsController.java
@@ -1,5 +1,71 @@
 package com.example.Controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.example.Procedure;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+
 public class ProcedureDetailsController {
-    
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button managePatientBtn;
+
+    @FXML
+    private TextField patInfoAge;
+
+    @FXML
+    private TextField patInfoContact;
+
+    @FXML
+    private TextField patInfoCourseID;
+
+    @FXML
+    private TextField patInfoDepartment;
+
+    @FXML
+    private TextField patInfoGender;
+
+    @FXML
+    private TextField patInfoHistory;
+
+    @FXML
+    private TextField patInfoID;
+
+    @FXML
+    private TextField patInfoName;
+
+    @FXML
+    private TextField patInfoProcedureID;
+
+    @FXML
+    private TableColumn<Procedure, String[]> prodDetMedNameCol;
+
+    @FXML
+    private TableView<Procedure> prodDetTable;
+
+    @FXML
+    private StackPane treatment_course_page;
+
+    @FXML
+    private Label unameLabel;
+
+    @FXML
+    void initialize() {
+
+    }
+
 }
