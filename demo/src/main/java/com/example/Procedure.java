23c3e8369b75fd5057a45d2d7e5156dc4bc41e1c
Tang Zhi Siang <siangztang@gmail.com>
2023-06-20 18:09:31 +0800

    add medicine list

--- demo/src/main/java/com/example/Procedure.java
+++ demo/src/main/java/com/example/Procedure.java
@@ -9,14 +9,16 @@ public class Procedure{
     private Date procedure_date;
     private Date procedure_time;
     private String treatment_course_id;
+    private String[] medicine_list;
 
 
-    public Procedure(String procedure_id, String type, Date procedure_date, Date procedure_time, String treatment_course_id) {
+    public Procedure(String procedure_id, String type, Date procedure_date, Date procedure_time, String treatment_course_id, String[] medicine_list) {
         this.procedure_id = procedure_id;
         this.type = type;
         this.procedure_date = procedure_date;
         this.procedure_time = procedure_time;
         this.treatment_course_id = treatment_course_id;
+        this.medicine_list = medicine_list;
     }
 
     public String getProcedure_id() {
@@ -37,5 +39,9 @@ public class Procedure{
 
     public String getTreatment_course_id() {
         return treatment_course_id;
+    }
+
+    public String[] getMedicine_list() {
+        return medicine_list;
     }
 }
