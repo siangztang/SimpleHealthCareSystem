d0ab6d6f29f5543801ef7e0bc9f2da5cf41014fa
Tang Zhi Siang <siangztang@gmail.com>
2023-06-22 19:25:25 +0800

    commit

--- demo/src/main/java/com/example/BloodAnalysis.java
+++ demo/src/main/java/com/example/BloodAnalysis.java
@@ -59,43 +59,43 @@ public class BloodAnalysis extends Analysis{
     }
 
     // Normal range: Varies with age and sex, but typically below 20 mm/hr for men and below 30 mm/hr for women
-    public int getESR() {
-        return ESR;
+        public int getESR() {
+            return ESR;
+        }
+        //Validation for Blood Analysis Form
+        public int validationBlood(double red_cells, double haemoglobin, String color, String parasites, int white_cells, int stab_neuthrophil, int lymphocytes, int ESR) {
+        //Validate Red Cells
+        if (red_cells >=4.5 || red_cells >=5.5) {
+            return 0;
+        }
+        //Haemoglobin
+        if(haemoglobin >= 13.5 || haemoglobin <=17.5){
+            return 0;
+        }
+        //piss color  
+        if(color !="" | color.matches("\b(red|bright red|purple)\b")){
+            return 0;
+        }
+        //parasites
+        if(parasites !="" | parasites.matches("\b(positive|negative)\b")){
+            return 0;
+        }
+        //white cells
+        if(white_cells >= 5000 || white_cells <= 11000){
+            return 0;
+        }
+        //stab neuthrophil
+        if(stab_neuthrophil >= 0 || stab_neuthrophil <= 3){
+            return 0;
+        }
+        //lymphocytes
+        if(lymphocytes >= 20 || lymphocytes <= 40){
+            return 0;
+        }
+        //ESR
+        if(ESR >= 20 || ESR <= 40){
+            return 0;
+        }
+        return 1;
     }
-    //Validation for Blood Analysis Form
-    public int validationBlood(double red_cells, double haemoglobin, String color, String parasites, int white_cells, int stab_neuthrophil, int lymphocytes, int ESR) {
-    //Validate Red Cells
-    if (red_cells >=4.5 || red_cells >=5.5) {
-        return 0;
-    }
-    //Haemoglobin
-    if(haemoglobin >= 13.5 || haemoglobin <=17.5){
-        return 0;
-    }
-    //piss color  
-    if(color !="" | color.matches("\b(red|bright red|purple)\b")){
-        return 0;
-    }
-    //parasites
-    if(parasites !="" | parasites.matches("\b(positive|negative)\b")){
-        return 0;
-    }
-    //white cells
-    if(white_cells >= 5000 || white_cells <= 11000){
-        return 0;
-    }
-    //stab neuthrophil
-    if(stab_neuthrophil >= 0 || stab_neuthrophil <= 3){
-        return 0;
-    }
-    //lymphocytes
-    if(lymphocytes >= 20 || lymphocytes <= 40){
-        return 0;
-    }
-    //ESR
-    if(ESR >= 20 || ESR <= 40){
-        return 0;
-    }
-
-    return 1;
 }
