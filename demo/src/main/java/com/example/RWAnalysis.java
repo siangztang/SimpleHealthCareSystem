f6d24668340e8b9c3d13973d8f316a3b70beaf71
Jiun01 <54741236+Jiun01@users.noreply.github.com>
2023-06-25 17:11:55 +0800

    add parameter type

--- demo/src/main/java/com/example/RWAnalysis.java
+++ demo/src/main/java/com/example/RWAnalysis.java
@@ -1,7 +1,7 @@
 package com.example;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+//import java.time.LocalDate;
+//import java.time.format.DateTimeFormatter;
 
 public class RWAnalysis extends Analysis{
     
